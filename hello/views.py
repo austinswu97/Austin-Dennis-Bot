6250a7537284c6bda958287d81eb2e6b9d748881
austinswu97 <aw2793@nyu.edu>
2016-05-29 15:36:45 -0700

    whatever

--- hello/views.py
+++ hello/views.py
@@ -13,11 +13,11 @@ import json
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello from Python! Austin')
+#   return HttpResponse('Hello from Python! Austin')
     #return render(request, 'index.html')
-#    r = requests.get('http://httpbin.org/status/418')
-#    print r.text
-#    return HttpResponse('<pre>' + r.text + '</pre>')
+    r = requests.get('http://httpbin.org/status/418')
+    print r.text
+    return HttpResponse('<pre>' + r.text + '</pre>')
 
 @csrf_exempt
 def webhook(request):
